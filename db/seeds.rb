6f30658bc10466ca3ecd9ca5013660df98dafd4c
aa1b02ab7492d696da6e8ac540ffa187ba623c88e15238d8d026f79bc5d39b3c <darrickpang@DESKTOP-02GI1R6.localdomain>
2020-05-06 11:42:23 -0700

    darrick seed file

---
+++ db/seeds.rb
@@ -0,0 +1,91 @@
+
+Ingredient.create([
+    {
+        ingredient_name: "carne asada"
+        ingredient_type: "filling"
+    },
+    {
+        ingredient_name: "al pastor"
+        ingredient_type: "filling"
+    },
+    {
+        ingredient_name: "carnitas"
+        ingredient_type: "filling"
+    },
+    {
+        ingredient_name: "battered fish"
+        ingredient_type: "filling"
+    },
+    {
+        ingredient_name: "chile verde"
+        ingredient_type: "filling"
+    },
+    {
+        ingredient_name: "chicken"
+        ingredient_type: "filling"
+    },
+    {
+        ingredient_name: "beans and rice"
+        ingredient_type: "filling"
+    },
+    {
+        ingredient_name: "flour tortilla"
+        ingredient_type: "shell"
+    },
+    {
+        ingredient_name: "soft corn"
+        ingredient_type: "shell"
+    },
+    {
+        ingredient_name: "bowl"
+        ingredient_type: "shell"
+    },
+    {
+        ingredient_name: "tortilla chips"
+        ingredient_type: "shell"
+    },
+    {
+        ingredient_name: "hard corn"
+        ingredient_type: "shell"
+    },
+    {
+        ingredient_name: "salsa roja"
+        ingredient_type: "toppings"
+    },
+    {
+        ingredient_name: "salsa verde"
+        ingredient_type: "toppings"
+    },
+    {
+        ingredient_name: "pico de gallo"
+        ingredient_type: "toppings"
+    },
+    {
+        ingredient_name: "lettuce"
+        ingredient_type: "toppings"
+    },
+    {
+        ingredient_name: "tomato"
+        ingredient_type: "toppings"
+    },
+    {
+        ingredient_name: "cheese"
+        ingredient_type: "toppings"
+    },
+    {
+        ingredient_name: "guacamole"
+        ingredient_type: "toppings"
+    },
+    {
+        ingredient_name: "jalapeno"
+        ingredient_type: "toppings"
+    },
+    {
+        ingredient_name: "cilantro"
+        ingredient_type: "toppings"
+    },
+    {
+        ingredient_name: "onions"
+        ingredient_type: "toppings"
+    }
+])
